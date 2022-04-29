9ed04da3b444531f8eec98ccc708df512ee31edb
wiktor34306 <92991883+wiktor34306@users.noreply.github.com>
2022-04-29 19:59:02 +0200

    mojcommit

---
+++ L8_PR-main/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QuizComponent from "./components/QuizComponent";
+import Test1 from "./components/1-componentClass";
+import Test2 from "./components/2-componentFunctional";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <QuizComponent/>
+  );
+}
+
+export default App;
---
+++ L8_PR-main/src/components/QuizComponent.jsx
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import Question from "./question";
+import Answers from "./answers";
+import Results from "./results";
+import Actions from "./actions";
+
+
+//pobranie pytań z pliku JSON
+import questions from '../questions.json';
+
+const styles = {
+   display: 'flex',
+   justifyContent: 'center'
+};
+
+const QuizComponent = (props) => {
+    //stworzenie hooków
+
+    const [currentIndex, setIndex] = useState(0);
+    const [currentQuestion, setQuestion] = useState(questions[currentIndex]);
+    const [currentPoints, setPoints] = useState(0);
+    const [allowToChoose, changePermission] = useState(true);
+    const [markedAnswer, markAnswer] = useState({key: -1, variant: ''});
+
+    //przejscie do kolejnego pytania
+
+    const handleNextQuestion = () => {
+        const nextValue = currentIndex + 1;
+        if (nextValue > questions.length - 1) {
+            setIndex(questions.length - 1);
+            return;
+        }
+        setIndex(nextValue);
+        setQuestion(questions[nextValue]);
+        changePermission(true);
+        markAnswer({key: -1, variant: ''});
+    };
+// przejście do poprzedniego pytania
+    const handlePrevQuestion = () => {
+        const prevValue = currentIndex - 1;
+        if (prevValue < 0) {
+            setIndex(0);
+            return;
+        }
+        setIndex(prevValue);
+        setQuestion(questions[prevValue]);
+        changePermission(true);
+        markAnswer({key: -1, variant: ''});
+    };
+
+// sprawdzenie poprawnej odpowiedzi
+
+    const handleCheckAnswer = (chosenOption, key) => {
+        if (!allowToChoose) {
+            return;
+        }
+        if (currentQuestion.correct_answer === chosenOption) {
+            const points = currentPoints + 1;
+            setPoints(points);
+            changePermission(false);
+            markAnswer({key, variant: 'bg-success'})
+        } else {
+            changePermission(false);
+            markAnswer({key, variant: 'bg-danger'})
+        }
+    };
+
+    // wyświetlenie zawartosci
+
+    return (
+    <div style={styles}>
+        <div className="container">
+            <Question
+                className="col-12"
+                currentQuestion={currentQuestion.question}
+                currentIndex={currentIndex + 1}
+                allQuestions={questions.length}
+            >
+            </Question>
+            <Answers className="col-12"
+                     checkAnswer={handleCheckAnswer}
+                     currentAnswers={currentQuestion.answers}
+                     markedAnswer={markedAnswer}
+            />
+            <Results points={currentPoints}/>
+            <Actions
+                disablePrev={currentIndex > 0}
+               disableNext={currentIndex !== questions.length - 1}
+                prev={handlePrevQuestion}
+                next={handleNextQuestion}
+            />
+
+        </div>
+        </div>
+    )
+};
+
+export default QuizComponent;
